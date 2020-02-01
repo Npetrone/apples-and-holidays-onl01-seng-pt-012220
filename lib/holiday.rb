b69c528c361ea4a68eadb1e114e1f0a772d86cd8
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-01 03:44:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,6 +50,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  
+  
 
 end
 
@@ -61,6 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  
 
 end
 
