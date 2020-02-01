52151bd1dae5861c17a5795da0126dc74bee0a63
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-01 03:45:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,7 +26,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   
   holiday_hash[:winter].each do |holiday, decorations|
-   decorations.push("Balloons")
+   decorations.push(supply)
   end
 
 end
